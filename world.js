4147a354270c996ec678037b213a336d4081c60f
JantaeLeckie <jaleckiie22@gmail.com>
2020-11-17 20:27:39 -0500

    Exercise 3

---
+++ world.js
@@ -0,0 +1,27 @@
+window.onload = mystarter;
+function mystarter(){
+    let searchbtn = document.getElementById('lookup1');
+    let searchbtn2 = document.getElementById('lookup2');
+    let msg = document.getElementById('result');
+    searchbtn.addEventListener('click', function(element) {
+        element.preventDefault();
+        var myform = document.getElementById("country").value; 
+        //do
+        console.log("This is 1");
+        fetch("world.php"+"? country="+myform)
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                return Promise.reject('something went wrong!')
+            }
+        })
+        .then(data => {
+            msg.innerHTML = data;
+        })
+        .catch(error => console.log('There was an error: ' + error));
+           
+    });
+
+    
+}
--- world.php
+++ world.php
@@ -3,15 +3,29 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
+$mys = 'is ruled by';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+//$stmt = $conn->query("SELECT * FROM countries");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
+//$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
+$coun = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+$results = $coun->fetchAll(PDO::FETCH_ASSOC);
+echo $country;
+// $mycity = $conn->query("SELECT cities.name, cities.district, cities.population
+//           FROM cities join countries on cities.country_code=countries.code
+//           WHERE countries.name='$country'");
+
+
+// $city = $mycity->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
-<ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+
+
+
