1433d1c90ee4a504d9014644573a5132b665bebe
JantaeLeckie <jaleckiie22@gmail.com>
2020-11-17 21:02:57 -0500

    Exercise 4

--- world.js
+++ world.js
@@ -23,5 +23,24 @@ function mystarter(){
            
     });
 
-    
+    searchbtn2.addEventListener('click', function(element) {
+        element.preventDefault();
+        var myform = document.getElementById("country").value; 
+        var cities = document.getElementById("country").value; 
+        //do
+        console.log("This is 2");
+        fetch("world.php"+"?country="+myform +"&context=cities")
+        .then(response => {
+            if (response.ok) {
+                return response.text()
+            } else {
+                return Promise.reject('something went wrong!')
+            }
+        })
+        .then(data => {
+            msg.innerHTML = data;
+        })
+        .catch(error => console.log('There was an error: ' + error));
+           
+    });
 }
--- world.php
+++ world.php
@@ -10,31 +10,33 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 
 //$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $country = filter_input(INPUT_GET, "country", FILTER_SANITIZE_STRING);
-//$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
+$context = filter_input(INPUT_GET, "context", FILTER_SANITIZE_STRING);
 $coun = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $coun->fetchAll(PDO::FETCH_ASSOC);
 echo $country;
-// $mycity = $conn->query("SELECT cities.name, cities.district, cities.population
-//           FROM cities join countries on cities.country_code=countries.code
-//           WHERE countries.name='$country'");
+$mycity = $conn->query("SELECT cities.name, cities.district, cities.population
+          FROM cities join countries on cities.country_code=countries.code
+          WHERE countries.name='$country'");
 
 
-// $city = $mycity->fetchAll(PDO::FETCH_ASSOC);
+$city = $mycity->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
 <!-- <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?> -->
-<?php ?>
+
+
+<?php if(!isset($context)):?>
   <table class = "display">
     <caption><h2>TABLE SHOWING COUNTRIES</h2></caption>
     <thead>
       <tr>
         <th class = "th1">Name</th>
-        <th class = "th2">Continent</th>
-        <th class = "th3">Independence</th>
-        <th class = "th4">Name of State</th>
+        <th class = "th1">Continent</th>
+        <th class = "th1">Independence</th>
+        <th class = "th1">Name of State</th>
       </tr>
     </thead>
     <tbody>
@@ -48,6 +50,25 @@ echo $country;
       <?php endforeach; ?>
     </tbody>
   </table>
-<?php ?>
-
-
+<?php endif; ?>
+<?php if(isset($context)):?>
+  <table class = "display">
+    <caption><h2>TABLE SHOWING CITIES</h2></caption>
+    <thead>
+      <tr>
+        <th class = "th1">Name</th>
+        <th class = "th1">District</th>
+        <th class = "th1">Population</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($city as $city): ?>
+        <tr>
+          <td><?php echo $city['name']; ?></td>
+          <td><?php echo $city['district']; ?></td>
+          <td><?php echo $city['population']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php endif; ?>
