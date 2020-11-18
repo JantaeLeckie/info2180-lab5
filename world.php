fbd7eda4a3e8c27664ec9690fb2d85be84aad1a8
JantaeLeckie <jaleckiie22@gmail.com>
2020-11-17 20:41:17 -0500

    Exercise 4

--- world.php
+++ world.php
@@ -23,9 +23,31 @@ echo $country;
 
 
 ?>
-<?php foreach ($results as $row): ?>
+<!-- <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-
+<?php endforeach; ?> -->
+<?php ?>
+  <table class = "display">
+    <caption><h2>TABLE SHOWING COUNTRIES</h2></caption>
+    <thead>
+      <tr>
+        <th class = "th1">Name</th>
+        <th class = "th2">Continent</th>
+        <th class = "th3">Independence</th>
+        <th class = "th4">Name of State</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($results as $country): ?>
+        <tr>
+          <td><?php echo $country['name']; ?></td>
+          <td><?php echo $country['continent']; ?></td>
+          <td><?php echo $country['independence_year']; ?></td>
+          <td><?php echo $country['head_of_state']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+<?php ?>
 
 
